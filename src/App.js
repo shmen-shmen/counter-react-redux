8d494cf8b97140d8c26045b1193158469ec6cc9c
shmen-shmen <akshevchenko@proton.me>
2023-01-13 17:09:12 +0600

    app without redux

--- src/App.js
+++ src/App.js
@@ -1,58 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	const [stateObj, setStateObj] = useState({
+		days: 11,
+		hours: 31,
+		minutes: 27,
+		seconds: 11,
+		activeSession: "minutes",
+	});
+
+	const addOrSubtract = (e) => {
+		const session = stateObj["activeSession"];
+		let number = stateObj[session];
+		let newStateObj = { ...stateObj };
+		if (e.target.getAttribute("data-todo") === "add") {
+			newStateObj[session] = number + 1;
+		} else {
+			newStateObj[session] = number - 1;
+		}
+		setStateObj(newStateObj);
+	};
+
+	return (
+		<section className="app-section">
+			<section className="header-section">
+				<h3>BOOK NAME :</h3>
+				<h1 style={{ backgroundColor: "white", color: "var(--dark-color)" }}>
+					Understanding Redux - 1
+				</h1>
+				<h3>TOTAL TIME SPENT ON PROJECT :</h3>
+			</section>
+			<Counter stateObj={stateObj} setStateObj={setStateObj}></Counter>
+			<section className="buttons-wrapper">
+				<button onClick={addOrSubtract} data-todo={"add"}>
+					more▲▲▲
+				</button>
+				<button onClick={addOrSubtract} data-todo={"subtract"}>
+					less▽▽▽
+				</button>
+			</section>
+		</section>
+	);
 }
+
+const Counter = (props) => {
+	const timeValues = Object.keys(props.stateObj).filter(
+		(key) => key !== "activeSession"
+	);
+	const sessionSelector = (e) => {
+		const newSession = e.target.innerHTML;
+		const newStateObj = { ...props.stateObj };
+		newStateObj["activeSession"] = newSession;
+		props.setStateObj(newStateObj);
+	};
+	return (
+		<section className="counter-section">
+			<div className="time-values-wrapper">
+				{timeValues.map((value) => {
+					return (
+						<div className="time-value" key={Math.random().toString()}>
+							<p className="big-number">{props.stateObj[value]}</p>
+							<p
+								className="number-label"
+								onClick={sessionSelector}
+								style={
+									value === props.stateObj["activeSession"]
+										? { color: "red" }
+										: { color: "white" }
+								}
+							>
+								{value}
+							</p>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
+};
 
 export default App;
